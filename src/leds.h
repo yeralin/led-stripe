05e47e33f519868cf95b2f0cd26c247dabd5b37f
Daniyar Yeralin <daniyar.yeralin@gmail.com>
2024-06-30 13:21:06 -0400

    Reduce # of leds for new stripes

--- src/leds.h
+++ src/leds.h
@@ -3,7 +3,7 @@
 // Information about the LED strip itself
 #define LED_PIN 27
 #define INITIAL_BRIGHTNESS 128
-#define NUM_LEDS    300
+#define NUM_LEDS    80
 #define CHIPSET     WS2812B
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
